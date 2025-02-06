c7d56f6e14198c78c518ec806b264fdcef370f73
xmiguelcastillo@gmail.com <xmiguelcastillo@gmail.com>
2025-02-05 20:50:11 -0500

    Initial commit

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -4,3 +4,14 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            seen[nums[i]] = i
+        return []
